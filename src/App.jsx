2079b032c035fed26f1251963ce4feebe8287638
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-01-15 10:36:39 +0600

    input form added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ function App() {
   //let counter=0;
   //console.log ("counter", counter);
   const [counter, setCounter]=React.useState(0);
+  const [text, setText]=React.useState("First text");
   const handleClick = () => {
     setCounter(counter+1);
     console.log("Counter value", counter);
   };
+  const handleChange = (event) => {
+    setText(event.target.value)
+  };
+  
   return (
     <div>
-      <p>{ counter } </p>
-      <button onClick={handleClick}> Add one</button>
-    </div>
+      <div>
+        <p>{ counter } </p>
+        <button onClick={handleClick}> Add one</button>
+      </div>
+
+      <div>
+      // input text form to input text and display it
+      <p>{text}</p>
+      <input type="text" value={text} onChange={handleChange} />
+      </div> 
+    </div>  
     );
 }
 export default App;
