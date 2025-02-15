ac59d110f25abb8683ccef4423114284b85f3ea0
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-02-15 14:22:09 +0600

    react element creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//import { StrictMode } from 'react'
+//import { createRoot } from 'react-dom/client'
+//import './index.css'
+//import App from './App.jsx'
+import React, {component} from 'react';
+import { render } from 'react-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+//createRoot(document.getElementById('root')).render(
+  /*<StrictMode>
     <App />
   </StrictMode>,
-)
+  
+ )*///;
+ const node=document.getElementById('root');
+ const root=React.createElement('div',{},
+  React.createElement('h1',{},'Hello World'),
+  React.createElement('p',{},'This is a paragraph'),
+  React.createElement('ul',{},
+  React.createElement('li',{},'Item 1'),
+  React.createElement('li',{},'Item 2')
+  )
+  );
+  render(root,node);
+  
