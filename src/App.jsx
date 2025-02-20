30075f7a90436e198d62cbc0966739514dcf3caa
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-02-20 16:39:11 +0600

    state example by on button click user name changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
-// function to fetch data from the API
-function App() {
-  // usage of useState for data manipulation
-  const [data, setData]=React.useState(null);
-  // usage of useEffect for side effects
-  useEffect(() => {fetch("https://jsonplaceholder.typicode.com/todos?_limit=5"). then((response)=>response.json()).then((data)=>setData(data))}, []);
-  
-  
-  return (
-    <div>
-      {/*<pre> {JSON.stringify(data, null,5)} </pre> */}
-      // map fetching data to display in the browser
-      {data && data.map((item) => (
-        <div key={item.id}>
-          <h1>{item.title}</h1>
-          <p>{item.completed ? "Completed" : "Not Completed"}</p>
-        </div>
-      ))}
-    </div>  
+import React from "react";
+import { render} from "react-dom";
+
+class Dutton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "mark"
+    }
+    this.onButtonClick=this.onButtonClick.bind(this);
+  }
+  onButtonClick() {
+    this.setState({
+      name: "john"
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>My name is {this.state.name}</h1>
+        <button onClick={this.onButtonClick}>reveal</button>
+      </div>
     );
+  }
 }
-export default App;
+export default Dutton;
+        
+     
+        
