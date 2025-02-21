062ba626b01faf7cc842d748cc64784871ce1011
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-02-21 21:11:21 +0600

    exploring lifecycle through parent children relation

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,48 @@ import React from "react";
 import { render} from "react-dom";
 import PropTypes from "prop-types";
 
-const UserProfile=props=>{
-    return <img src={`https://source.unsplash.com/${props.username}`}/>
-}
-UserProfile.propTypes={
-    username:PropTypes.string
-}
-UserProfile.defaultProps={
-    username:"mostafijcoder"
-}
-const UserProfileLink=props=>{
-    return <a href={`https://ifelse.io/${props.username}`}>{props.username}</a>
-}
-const UserCard=props=>{
+class ChildComponent extends React.Component {
+  static propTypes = {
+    name: PropTypes.string
+  };
+  static defaultProps=(function(){
+        console.log("ChildComponent: defaultProps");
+        return {};
+    }
+  )();
+  constructor(props) {
+    super(props);
+    console.log("ChildComponent: state");
+  }
+  render() {
+    console.log("ChildComponent: render");
     return (<div>
-        <UserProfile username={props.username}/>
-        <UserProfileLink username={props.username}/>
-    </div>
-    )
+      Name: {this.props.name}</div>
+    );
+  }
+};
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    }
+    this.onInputchange=this.onInputchange.bind(this);
+  }
+  onInputchange(e){
+    this.setState({name: e.target.value});
+  }
+  render() {
+    console.log("ParentComponent: render");
+    return (<div>
+      <input type="text" value={this.state.name} onChange={this.onInputchange}/>
+      <ChildComponent name={this.state.name}/>
+    </div>);
+  }
 }
+export default ParentComponent;
+  
+
+  
 
 
-
-export default UserCard;
-        
-     
-        
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 //import Counter from "./App.jsx";
-import UserCard from "./App.jsx";
+import ParentComponent from "./App.jsx";
 
 const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
   <React.StrictMode>
-    <UserCard username="mostafijcoder" />
+    <ParentComponent/>
   </React.StrictMode>
 );
